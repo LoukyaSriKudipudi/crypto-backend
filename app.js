a4f4c1c9eece0cd679dd208a5d6416c699a2a616
Loukya Sri <loukyasrikudipudi@gmail.com>
2025-08-31 17:12:35 +0530

    download link correction in protocol

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cryptoRoutes = require("./routes/cryptoRoutes");
 const cors = require("cors");
-app.set("trust proxy", true);
+app.set("trust proxy", 1);
 
 app.use(cors());
 app.use("/api", cryptoRoutes);
